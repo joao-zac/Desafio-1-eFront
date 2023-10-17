4b61ea9beccec0e1d335113efc2f1e6219116714
João Zacarias <102718142+joao-zac@users.noreply.github.com>
2023-10-17 01:03:08 -0300

    ✨feat: Pegando dados do input

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -5,20 +5,35 @@ import '../types/typesprops'
 const Form:React.FC<FormProps> = ({ enabled, setEnabled })=> {
 
   const [emailInputId, nomeInputId] = useId();
-  const [valid, setValid] = useState<boolean>();
+  const [data, setData] = useState<object>({nome:'', email:''});
   
   return (
     <form>
         
       <label htmlFor={nomeInputId}>Nome</label>
-      <input id={nomeInputId} name="nome" type="text" />
-  
+      <input 
+      id={nomeInputId} 
+      name="nome" 
+      type="text" 
+      onMouseOut={ 
+        (e) => {setData({nome:(e.target as HTMLInputElement).value })
+      }} 
+      />
+
       <label htmlFor={emailInputId}>Email</label>
-      <input id={emailInputId} name="email" type="email" />
+      <input 
+      id={emailInputId} 
+      name="email" 
+      type="email" 
+      onMouseOut={ 
+        (e) => {setData({nome:(e.target as HTMLInputElement).value })
+      }} 
+      />
         
       <button onClick={(e) => {
         setEnabled(!enabled) 
         e.preventDefault()
+        console.log(data)
       }}
       >Enviar</button>
       
